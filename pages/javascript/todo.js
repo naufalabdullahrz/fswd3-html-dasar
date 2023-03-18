236338d4e67864aa4ca59f06345d8637f937462d
Naufal Abdullah Rasyiq Zaki <122874185+naufalabdullahrz@users.noreply.github.com>
2023-03-18 09:49:44 +0700

    Add files via upload

---
+++ pages/javascript/todo.js
@@ -0,0 +1,85 @@
+let todos = {}
+const STORAGE_DO = "STORAGE_DO"
+const todoBox = document.getElementById('todo')
+
+//membaca localStorage 
+if(todoFromLocal = localStorage.getItem(STORAGE_DO)) {
+    todos = JSON.parse(todoFromLocal)
+    for(let key in todos)
+        createList(key, todos[key])
+        }
+
+
+    function syncLocalStorage(activity, item, status = false) {
+        switch(activity) {
+            case 'ADD':
+            case 'UPDATE':
+                todos[item] = status
+                break;
+            case 'DELETE':
+                delete todos[item]
+                break;
+            default:
+                break;    
+            }
+
+        localStorage.setItem(STORAGE_DO, JSON.stringify(todos))
+            return
+        }
+
+       // TODO 
+function add() {
+    let textbaru = document.getElementById('dobaru')
+    createList(textbaru.value)
+    syncLocalStorage('ADD', textbaru.value)    
+    textbaru.value = ''
+}
+
+function createList(text, status = false) {
+    let listdone = (status) ? 'text-decoration-line-through' : ''
+
+    let todoBaru = `<li> 
+                    <span class='${listdone}' onclick='toggle(this)'>${text}</span>
+                    <span onclick='removeItem(this)'>x</span>
+                    </li>`
+
+            todoBox.insertAdjacentHTML('afterbegin', todoBaru);
+        }
+
+//Toggle/coret
+function toggle(elm) {
+    let status = elm.classList.toggle('text-decoration-line-through')
+     syncLocalStorage('UPDATE', elm.innerText, status)    
+}
+
+//Delete
+function removeItem(elm) {
+    elm.parentElement.remove()
+    syncLocalStorage('DELETE', elm.previousElementSibling.innerText.trim())    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
